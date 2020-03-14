956735f545609cda01e42bbb53037074099c936a
AndreiBy <andreialshuk@gmail.com>
2020-03-14 11:08:02 +0300

    метод умножение

--- src/com/myapp/Calculator.java
+++ src/com/myapp/Calculator.java
@@ -2,8 +2,12 @@ package com.myapp;
 
 public class Calculator {
 
-    public static double minus(double first, double second){
+    public static double minus(double first, double second) {
         int x;
         return 0.0;
+    }
+
+    public static double multiplay(double first, double second) {
+        return first*second;
     }
 }
